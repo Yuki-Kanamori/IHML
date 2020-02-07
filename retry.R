7c1d824ea961294e01da9f3f1243ecbc14847dbe
Yuki-Kanamori <y.kanamory@gmail.com>
2020-02-08 07:11:47 +0900

    initil

---
+++ retry.R
@@ -0,0 +1,158 @@
+
+# packages ------------------------------------------------------
+library(CompRandFld)
+library(RandomFields)
+library(mapproj)
+library(fields)
+require(tidyverse)
+
+
+
+
+pred.y <- pred.a <- NULL      # true contribution
+res.y <- res.a <- NULL       # contribution estimated by ML
+
+
+# simulated data ------------------------------------------------
+# env(s,t) = env(t) + env(s) (+ env(s,t))
+# 
+# env(t) ~ N(mu, sigma)
+# mu_sst = 5+0.4*Year / mu_sal = 10+0.2*Year
+# 
+# env(s) ~ MVN(0, Σ)
+# ΣはMatérn関数(RFノイズはありで，localノイズは無し)
+
+### SST(t)
+n.year <- 50     # number of year
+year <- 1:n.year    # year index
+a.sst <- 5    #   intercept of sst 
+b.sst <- 0.4     #   trend of sst
+sigma.sst <- 3     # variation in sst
+
+sst_t <- rnorm(n.year, a.sst + b.sst*year, sigma.sst)     # sst realization
+sst_t = (sst_t - mean(sst_t))/sd(sst_t)
+
+### SST(s)
+# Define the spatial-coordinates of the points:
+set.seed(261)
+x = rep(1:15)
+y = rep(1:15)
+
+size = length(x)
+size*size
+
+# Simulation of a spatial Gaussian random field:
+CorrelationParam("matern")
+matern_sst <- RFsim(x, y, corrmodel = "matern", grid = TRUE, 
+                    param = list(smooth = 1, mean = 0, sill = 0.5, scale = 1, nugget = 0))$data
+
+### heat map
+mat_sst = matern_sst %>% data.frame() %>% gather(key = x, value = sst, 1:size) %>% 
+  mutate(x = rep(1:size, each = size), y = rep(seq(1, size, 1), size))
+summary(mat_sst)
+mat_sst$sst = scale(mat_sst$sst)
+colnames(mat_sst)
+
+require(ggplot2)
+g = ggplot(mat_sst, aes(x = x, y = y, fill = sst))
+t = geom_tile()
+c = scale_fill_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
+g+t+c+theme_bw()+labs(fill = "Scaled SST")
+
+sst_s = mat_sst$sst
+
+### SST(s,t)
+# Define the spatial-coordinates of the points:
+x = rep(1:15)
+y = rep(1:15)
+
+size = length(x)
+size*size
+
+# Simulation of a spatial Gaussian random field:
+CorrelationParam("matern")
+matern_sst <- RFsim(x, y, corrmodel = "matern", grid = TRUE, 
+                    param = list(smooth = 1, mean = 0, sill = 0.5, scale = 1, nugget = 0))$data
+
+sst_st = matrix(NA, nrow = size*size, ncol = n.year)
+rand = floor(runif(n.year, 0, 1000))
+for(i in 1:n.year){
+  set.seed(rand[i])
+  matern = RFsim(x, y, corrmodel = "matern", grid = TRUE, 
+              param = list(smooth = 1, mean = 0, sill = 0.5, scale = 1, nugget = 0))$data
+  eps = matern %>% data.frame() %>% gather(key = x, value = sst, 1:size) %>% 
+    mutate(x = rep(1:size, each = size), y = rep(seq(1, size, 1), size))
+  
+  sst_st[, i] = eps$sst
+}
+
+
+
+### sal(t)
+n.year <- 50     # number of year
+year <- 1:n.year    # year index
+a.sal <- 10     #  intercept of sal
+b.sal <- 0.2    #    trend of sal
+sigma.sal <- 2    # variation in sal
+
+sal_t <- rnorm(n.year, a.sal+b.sal*year, sigma.sal)       # sal realization
+sal_t = (sal_t - mean(sal_t))/sd(sal_t)   # normalization
+
+# sal(s)
+# Define the spatial-coordinates of the points:
+set.seed(260)
+x = rep(1:15)
+y = rep(1:15)
+
+size = length(x)
+size*size
+
+# Simulation of a spatial Gaussian random field:
+CorrelationParam("matern")
+matern_sal <- RFsim(x, y, corrmodel = "matern", grid = TRUE, 
+                    param = list(smooth = 1, mean = 0, sill = 0.5, scale = 1, nugget = 0))$data
+
+### heat map
+mat_sal = matern_sal %>% data.frame() %>% gather(key = x, value = sal, 1:size) %>% 
+  mutate(x = rep(1:size, each = size), y = rep(seq(1, size, 1), size))
+summary(mat_sal)
+mat_sal$sal = scale(mat_sal$sal)
+colnames(mat_sal)
+
+require(ggplot2)
+g = ggplot(mat_sal, aes(x = x, y = y, fill = sal))
+t = geom_tile()
+c = scale_fill_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
+g+t+c+theme_bw()+labs(fill = "Scaled sal")
+
+sal_s = mat_sal$sal
+
+### sal(s,t)
+# Define the spatial-coordinates of the points:
+x = rep(1:15)
+y = rep(1:15)
+
+size = length(x)
+size*size
+
+# Simulation of a spatial Gaussian random field:
+CorrelationParam("matern")
+matern_sst <- RFsim(x, y, corrmodel = "matern", grid = TRUE, 
+                    param = list(smooth = 1, mean = 0, sill = 0.5, scale = 1, nugget = 0))$data
+
+sal_st = matrix(NA, nrow = size*size, ncol = n.year)
+rand = floor(runif(n.year, 0, 1000))
+for(i in 1:n.year){
+  set.seed(rand[i])
+  matern = RFsim(x, y, corrmodel = "matern", grid = TRUE, 
+                 param = list(smooth = 1, mean = 0, sill = 0.5, scale = 1, nugget = 0))$data
+  eps = matern %>% data.frame() %>% gather(key = x, value = sal, 1:size) %>% 
+    mutate(x = rep(1:size, each = size), y = rep(seq(1, size, 1), size))
+  
+  sal_st[, i] = eps$sal
+}
+
+
+# make SST and SAL ----------------------------------------------
+
+
